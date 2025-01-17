5dec684cb8eaeaa1935202b063cd395ede1bcaab
Ruan <ruanf@m2north.com>
2025-01-17 12:53:14 +0100

    Textract response working. Wip on the logic now.

--- sam-app/text-recognition/app.py
+++ sam-app/text-recognition/app.py
@@ -9,11 +9,19 @@ import pytesseract
 
 client = boto3.client('textract')
 
+class OcrResult:
+    def __init__(self, text, confidence):
+        self.text = text
+        self.confidence = confidence
+
+
 def lambda_handler(event, context):
 
     print(json.dumps(event, indent=2))
 
     base64_body = event['body']
+
+    ocr_results = []
 
     image_data = base64.b64decode(base64_body)
 
@@ -21,27 +29,29 @@ def lambda_handler(event, context):
     image = Image.open(io.BytesIO(image_data))
 
     # Perform OCR using pytesseract
-    text = pytesseract.image_to_string(image)
+    #text = pytesseract.image_to_string(image)
 
-    print(f"{text=}")
+    #print(f"{text=}")
 
     # Perform OCR using textract
     try:
         response = client.detect_document_text(
             Document={
-                'Bytes': base64.b64decode(base64_body)
+                'Bytes': image_data
             }
         )
 
-        print(json.dumps(response))
+        line_blocks = [block for block in response['Blocks'] if block['BlockType'] == 'LINE']
+        # Making some assumptions here..
+        best_line = max(line_blocks, key=lambda block: block['Confidence'])
 
-        return json.dumps(response)
+        ocr_results.append(OcrResult(best_line['Text'], best_line['Confidence']))
 
     except Exception as e:
         print(e)
         pass
 
+    print(json.dumps(ocr_results))
 
-    # TODO: Perform confidence scores..
-
-    return None
+    # Return most confident result.
+    return max(ocr_results, key=lambda ocr_result: ocr_result.confidence)
