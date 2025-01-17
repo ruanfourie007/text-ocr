e70e9f44fd7b461b0f6c98f0040cb4aaadb3f74f
Ruan <ruanf@m2north.com>
2025-01-17 11:31:28 +0100

    Initial push

---
+++ sam-app/text-recognition-proxy/app.py
@@ -0,0 +1,44 @@
+import json
+
+import base64
+
+import boto3
+
+lambda_client = boto3.client('lambda')
+
+
+def is_valid_base64(content):
+    try:
+        decoded_data = base64.b64decode(content, validate=True)
+        if base64.b64encode(decoded_data).decode('utf-8') == content.strip():
+            return True
+    except Exception:
+        pass
+    return False
+
+
+def lambda_handler(event, context):
+    base64_body = event['body']
+
+    print(f"{base64_body=}")
+
+    text = ""
+    error = ""
+    is_body_valid = is_valid_base64(base64_body)
+
+    response = lambda_client.invoke(
+        FunctionName="sam-app-OcrTextractFunction-Es4oZGh8wtjf",
+        InvocationType='RequestResponse',
+        Payload=event
+    )
+
+    print(f"{response=}")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "text": text,
+            "valid_body": is_body_valid,
+            "error": error,
+        }),
+    }
